6cd1bdefe5d530883fdad9def31c151ae2901f91
Aiden Ramgoolam <aidenramgoolam@v1040-wn-rt-a-11-252.campus-dynamic.uwaterloo.ca>
2024-05-19 15:29:30 -0400

    ,,

--- server/api/main.py
+++ server/api/main.py
@@ -4,6 +4,43 @@ from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
 
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+
+embedding = OpenAIEmbeddings(openai_api_key="sk-proj-IOzFRrBYg586gxZgtNjRT3BlbkFJa4XWFpR9d6gdGrmV9AJS")
+db = Chroma(persist_directory="./chromadb",embedding_function=embedding)
+
+
+# Create retriever
+retriever = db.as_retriever()
+
+# Define prompt template
+template = """
+You are an assistant for question-answering tasks.
+Use the provided context to help you answer the following question:
+
+<context>
+{context}
+</context>
+
+Question: {input}
+"""
+
+# Create a prompt template
+prompt = ChatPromptTemplate.from_template(template)
+
+# Create a chain 
+doc_chain = create_stuff_documents_chain(llm, prompt)
+chain = create_retrieval_chain(retriever, doc_chain)
+
+
 app = FastAPI()
 
 origins = [
@@ -21,7 +58,13 @@ app.add_middleware(
 
 @app.get("/q/")
 async def read_q(question: str = ""):
-    return {"question": question}
+    # User query 
+    response = chain.invoke({"input": question})
+
+# Get the Answer only
+    response['answer']
+    print(response)
+    return {"question": response}
 
 @app.get("/sub/")
 async def read_s(subreddit: str = "cscareeradvice"):
